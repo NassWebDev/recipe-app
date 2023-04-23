7218b93dfc365fc04d46a35bf14a743afdab8996
NassKillers <mr.nedjar.nassim@gmail.com>
2023-04-23 15:50:26 +0200

    addrecipe component

---
+++ src/views/AddRecipe.vue
@@ -0,0 +1,74 @@
+<template>
+    <form @submit.prevent class="form">
+      <h1>Ajouter une recette</h1>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" required v-model="newRecipe.name"/>
+          <label>
+            Nom
+          </label>
+        </div>
+        <div class="input textarea"> 
+          <textarea type="text" required v-model="newRecipe.description"/>
+          <label>
+            Description
+          </label>
+        </div>
+        <div class="ingredients">
+          <div v-for="ingredient in newRecipe.ingredientsRow" :key="ingredient" class="input ingredient">
+            <input type="text" required v-model="newRecipe.ingredients[ingredient-1]"/>
+            <label>
+              Ingredient
+            </label>
+          </div>
+          <button @click="addIngredients" class="addingredients">
+            Ajouter Ingredient
+          </button>
+        </div>
+      </div>
+      <button @click="addRecipe" class="addrecipe">
+        Ajouter la recette
+      </button>
+    </form>
+</template>
+
+<script setup>
+
+import { reactive } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const newRecipe = reactive({
+                    name: "",
+                    description: "",
+                    ingredients: [],
+                    ingredientsRow: 1,
+                  })
+
+const addIngredients = (() => {
+  newRecipe.ingredientsRow++;
+});
+
+const addRecipe = (async () => {
+  if(!newRecipe.name || !newRecipe.description || newRecipe.ingredients.length == 0){
+    alert("Vous devez remplir les champs.")
+  }
+  else{
+    try{
+      const resp = await axios.post('http://localhost:3000/recipe', newRecipe);
+      console.log(resp.data);
+      Object.assign(newRecipe, {
+                    name: "",
+                    description: "",
+                    ingredients: [],
+                    ingredientsRow: 1,
+                  });
+      router.push('/')
+    }
+    catch(err){
+      console.error(err);
+    }
+  }
+})
+</script>
