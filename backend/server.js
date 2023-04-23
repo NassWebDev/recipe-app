a6b630c1187d45ba2594b0e3ee2df709662b5837
NassKillers <mr.nedjar.nassim@gmail.com>
2023-04-23 16:10:10 +0200

    add .env

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Running on port ${port}...`);
 })
