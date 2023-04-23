573ac05b5ead41d9ecff1e90d558c5803d8c55ed
NassKillers <mr.nedjar.nassim@gmail.com>
2023-04-23 17:09:52 +0200

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AllRecipe from '../views/AllRecipe.vue';
 import AddRecipe from '../views/AddRecipe.vue';
 import RecipeDetails from '../views/RecipeDetails.vue';
-import LoginPage from '../views/LoginPage.vue';
-import RegisterPage from '../views/RegisterPage.vue';
-import PasswordForgot from '../views/PasswordForgot.vue';
 
 
 const routes = [
@@ -13,34 +10,16 @@ const routes = [
     path: '/',
     name: 'AllRecipe',
     component: AllRecipe,
-    // meta: { requiresAuth: true }
   },
   {
     path: '/addrecipe',
     name: 'AddRecipe',
     component: AddRecipe,
-    // meta: { requiresAuth: true }
   },
   {
     path: '/:id',
     name: 'RecipeDetails',
     component: RecipeDetails,
-    // meta: { requiresAuth: true }
-  },
-  {
-    path: '/login',
-    name: 'LoginPage',
-    component: LoginPage
-  },
-  {
-    path: '/register',
-    name: 'RegisterPage',
-    component: RegisterPage
-  },
-  {
-    path: '/forgot',
-    name: 'PasswordForgot',
-    component: PasswordForgot
   }
 ]
 
@@ -48,17 +27,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
-
-// router.beforeEach((to, from, next) => {
-//   if (to.meta.requiresAuth && store.state.currentUid == null) {
-//     next('/login')
-//   }
-//   // else if(to.name == 'AllRecipe' && store.state.allrecipe.length <= 0){
-//   //   next('/addrecipe')
-//   // }
-//   else{
-//     next()
-//   }
-// })
 
 export default router
