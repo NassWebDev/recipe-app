943f14fe60a106698b214fba4e3817d131ca104c
NassKillers <mr.nedjar.nassim@gmail.com>
2023-04-26 14:24:40 +0200

    change port

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Running on port ${port}...`);
 })
