7808ea45d3a27d06062da2673afa0e1241090f21
NassKillers <mr.nedjar.nassim@gmail.com>
2023-05-12 20:02:32 +0200

    config auth

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const AppError = require('./utils/appError');
 
 const connectDB = require('./config/db');
 
-const routes = require('./routes/recipes.routes');
+const recipesRoutes = require('./routes/recipes.routes');
+const authRoutes = require('./routes/auth.routes');
+const usersRoutes = require('./routes/users.routes');
+const authController = require('./controllers/auth.controller');
 
 connectDB();
 
@@ -12,6 +16,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/recipes", routes);
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+
+app.use(authController.protect)
+
+app.use("/recipes", recipesRoutes);
+
+app.all("*", (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+})
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+})
 
 module.exports = app;
