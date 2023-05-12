dc91071f8d81205c15f1e958163197f1daa6322c
NassKillers <mr.nedjar.nassim@gmail.com>
2023-05-12 20:06:39 +0200

    add login-logout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <nav>
     <router-link to="/">Recettes</router-link>
-    <router-link to="/addrecipe">Ajouter une recette</router-link>
+    <router-link v-if="currentUser" to="/addrecipe">Ajouter une recette</router-link>
+    <router-link v-else to="/login" id="login">Login</router-link>
+    <button v-if="currentUser" @click="logout">Logout</button>
   </nav>
   <router-view/>
 </template>
 
+<script setup>
+import { computed } from 'vue';
+import { useAuthStore } from './store/auth';
+import { useRouter } from 'vue-router'
+const router = useRouter();
+
+const currentUser = computed(() => {
+  return useAuthStore().isAuthenticated;
+})
+
+const logout = (() => {
+  useAuthStore().logout();
+  localStorage.removeItem('user')
+  router.push('/login')
+})
+</script>
+
 <style>
+:root{
+  --green: #40926d;
+  --white: #ccc;
+  --blue: #20203b;
+  --dark-blue: #1a182b;
+}
+
 *{
   margin: 0;
   padding: 0;
@@ -18,7 +44,7 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #20203b;
+  background-color: var(--blue);
   min-height: 100vh;
 }
 
@@ -27,6 +53,7 @@ nav{
   padding: 20px 0;
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
 }
 
 nav a {
@@ -35,6 +62,15 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: var(--green);
+}
+
+#login, button {
+  border: 1px solid #ccc;
+  color: var(--white);
+  background-color: var(--green);
+  font-weight: bold;
+  padding: 5px 10px;
+  text-decoration: none;
 }
 </style>
