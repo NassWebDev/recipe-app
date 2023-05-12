e36aa90eff6a8634d10eda5656a88f6e610e67bc
NassKillers <mr.nedjar.nassim@gmail.com>
2023-05-12 19:59:29 +0200

    add user_id

--- api/models/recipe.model.js
+++ api/models/recipe.model.js
@@ -4,14 +4,19 @@ const recipeSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: true
+            required: [true, 'Ajouter un nom']
         },
         description: {
             type: String,
-            required: true
+            required: [true, 'Ajouter une description']
         },
         ingredients: {
             type: [String],
+            required: [true, 'Ajouter au moins un ingrédient']
+        },
+        user_id:{
+            type: String,
+            required: true
         }
     },
     {
