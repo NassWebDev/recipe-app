a73314dc0e292c8d6c30197833a7781fbcea0d4d
NassKillers <mr.nedjar.nassim@gmail.com>
2023-05-12 20:06:48 +0200

    create pinia

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 
+const pinia = createPinia();
+
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { RiEdit2Fill } from "oh-vue-icons/icons";
 
 addIcons(RiEdit2Fill); 
 
-createApp(App).use(createPinia()).use(router).component("fa", OhVueIcon).mount('#app')
+createApp(App).use(pinia).use(router).component("fa", OhVueIcon).mount('#app')
