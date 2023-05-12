f479ff3ac7ceb1281974afbef651ecd1d33fd43b
NassKillers <mr.nedjar.nassim@gmail.com>
2023-05-12 20:03:13 +0200

    lazy load components and protect vue routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,34 +4,60 @@ import AllRecipe from '../views/AllRecipe.vue';
 import AddRecipe from '../views/AddRecipe.vue';
 import RecipeDetails from '../views/RecipeDetails.vue';
 import EditRecipe from '../views/EditRecipe.vue';
+import SignUpPage from '../views/SignUpPage.vue';
+import LoginPage from '../views/LoginPage.vue';
+import { useAuthStore } from '../store/auth';
 
 
 const routes = [
   {
     path: '/',
     name: 'AllRecipe',
-    component: AllRecipe,
+    component: () => import('../views/AllRecipe.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/addrecipe',
     name: 'AddRecipe',
-    component: AddRecipe,
+    component: () => import('../views/AddRecipe.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/:id',
     name: 'RecipeDetails',
-    component: RecipeDetails,
+    component: () => import('../views/RecipeDetails.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/edit/:id',
     name: 'EditRecipe',
-    component: EditRecipe,
+    component: () => import('../views/EditRecipe.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/signup',
+    name: 'SignUpPage',
+    component: () => import('../views/SignUpPage.vue'),
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: () => import('../views/LoginPage.vue'),
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && useAuthStore().user == null) {
+    next('/login')
+  }
+  else{
+    next()
+  }
 })
 
 export default router
