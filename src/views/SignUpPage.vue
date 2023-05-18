a59e835e355619d36a8576ca99fb95a64a364372
NassKillers <mr.nedjar.nassim@gmail.com>
2023-05-18 17:57:43 +0200

    add errMsg

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -5,31 +5,32 @@
         </h1>
         <div class="inputs">
             <div class="input username">
-                <input type="text" required v-model="newUser.username" id="username">
+                <input type="text" required v-model="newUser.username" id="username" autocomplete="off">
                 <label for="username">
                     Nom:
                 </label>
+                <p class="errorMsg">{{ errMsg.username }}</p>
             </div>
             <div class="input email">
-                <input type="email" required v-model="newUser.email" id="email">
+                <input type="email" required v-model="newUser.email" id="email" autocomplete="off">
                 <label for="email">
                     Email:
                 </label>
+                <p class="errorMsg">{{ errMsg.email }}</p>
             </div>
             <div class="input password">
-                <input type="password" required v-model="newUser.password" id="password">
+                <input type="password" required v-model="newUser.password" id="password" autocomplete="off">
                 <label for="password">
                     Mot de passe:
                 </label>
+                <p class="errorMsg">{{ errMsg.password }}</p>
             </div>
             <div class="input password">
-                <input type="password" required v-model="newUser.passwordConfirm" id="passwordConfirm">
+                <input type="password" required v-model="newUser.passwordConfirm" id="passwordConfirm" autocomplete="off">
                 <label for="password">
                     Confirmer votre Mot de passe:
                 </label>
-            </div>
-            <div class="listErrors">
-                <p v-for="error in errorMsg">{{ error }}</p>
+                <p class="errorMsg">{{ errMsg.passwordConfirm }}</p>
             </div>
             <p>Vous avez déja un compte ? Allez <router-link to="/login">ici</router-link>.</p>
         </div>
@@ -40,7 +41,7 @@
 </template>
   
 <script setup>
-import { ref, reactive } from 'vue';
+import { reactive } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
     
@@ -55,7 +56,12 @@ import { useAuthStore } from '../store/auth';
         passwordConfirm: ""
     })
 
-const errorMsg = ref([])
+const errMsg = reactive({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: ""
+})
     
     const signup = (async () => {
         if(!newUser.username || !newUser.email || !newUser.password || !newUser.passwordConfirm){
@@ -76,7 +82,10 @@ const errorMsg = ref([])
                 useAuthStore().login(resp.data);
             }
             catch(err){
-                errorMsg.value = err.response.data.message
+                for (const error in err.response.data.message) {
+                    errMsg[error] = err.response.data.message[error];
+                }
+                console.log(errMsg);
             }
         }
     })
